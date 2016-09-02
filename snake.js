6d30956096bef0efefd979bd797367c39e626fb1
blaarg171 <blaarg171@users.noreply.github.com>
2016-09-02 15:18:50 -0500

    Started tile based system

--- snake.js
+++ snake.js
@@ -7,6 +7,20 @@ backBuffer.height = frontBuffer.height;
 var backCtx = backBuffer.getContext('2d');
 var oldTime = performance.now();
 
+var mTileSize = 20;
+var mTileX = backBuffer.height/mTileSize + 2; // 20px + 'wall'
+var mTileY = backBuffer.width/mTileSize + 2; // 20px + 'wall'
+var mTiles = new Array(mTileX);
+for(var x = 0; x < mTileX; x++) {
+	mTiles[x] = new Array(mTileY);
+}
+
+var mSnakeX = 8;
+var mSnakeY = 25;
+
+var mDirection = 'down';
+//var mSpeed;
+
 /**
  * @function loop
  * The main game loop.
@@ -58,7 +72,67 @@ function render(elapsedTime) {
   backCtx.clearRect(0, 0, backBuffer.width, backBuffer.height);
 
   // TODO: Draw the game objects into the backBuffer
+  
+  backCtx.fillRect(mSnakeX * 20, mSnakeY * 20, 20, 20);
 
+}
+
+function moveSnake() {
+	var lNextTile;
+	switch (mDirection) {
+		case "up":
+			lNextTile = mTiles[mSnakeX - 1][mSnakeY];
+			break;
+		
+		case "left":
+			lNextTile = [mSnakeX][mSnakeY - 1];
+			break;
+		
+		case "right":
+			lNextTile = [mSnakeX][mSnakeY + 1];
+			break;
+		
+		case "down":
+			lNextTile = [mSnakeX + 1][mSnakeY];
+			break;
+	}
+	
+	switch (lNextTile) {
+		
+	}
+}
+
+window.onkeydown = function (event) { // onkeypress?
+	switch (event.keyCode) {
+
+        //Up
+        case 38:
+        case 87:
+            event.preventDefault();
+            if (mDirection != "down") mDirection = "up";
+            break;
+
+        //Left
+        case 37:
+        case 65:
+            event.preventDefault();
+            if (mDirection != "right") mDirection = "left";
+            break;
+
+        //Right
+        case 39:
+        case 68:
+            event.preventDefault();
+            if (mDirection != "left") mDirection = "right";
+            break;
+
+        //Down
+        case 40:
+        case 83:
+            event.preventDefault();
+            if (mDirection != "up") mDirection = "down";
+            break;
+    }
 }
 
 /* Launch the game */
